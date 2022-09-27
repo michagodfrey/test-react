79dcd6518bcb268ada61449a879115e1b7fd3ed7
Michael Godfrey <m.godfrey59@gmail.com>
2022-09-28 06:56:24 +1200

    sass testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import Component from './Component'
-import Buttons from './Buttons'
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+import "./styles/Variables.css";
+import "./styles/Global.css";
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+
+
   return (
-    <div>
-      <Component />
-      <Buttons />
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
--- src/Component.js
+++
@@ -1,5 +0,0 @@
-function Component() {
-  return <h1>Step by step, I am getting familiar with React</h1>
-}
-
-export default Component;
